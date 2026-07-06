c832eaeaff9e88a89802c9e42ab0e2c6a6bc4186
Wachii <dani.alvarado.sosa@gmail.com>
2026-07-06 18:30:44 +0200

    -fixed f location and website name

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,11 +13,12 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <title>Farreo</title>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
-        <link rel="icon" type="image/png" href="/brand/farreo-f.png" />
-        <link rel="shortcut icon" href="/brand/farreo-f.png" />
-        <link rel="apple-touch-icon" href="/brand/farreo-f.png" />
+        <link rel="icon" type="image/png" href="/brand/farreo-f-favicon.png" />
+        <link rel="shortcut icon" href="/brand/farreo-f-favicon.png" />
+        <link rel="apple-touch-icon" href="/brand/farreo-f-favicon.png" />
       </head>
       <body className={`app-body ${inter.className}`}>
         <MusicPlayerProvider>
